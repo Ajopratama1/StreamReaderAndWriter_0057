465875c51e864bc5f68a9cdb6f7801afa85b8481
Ajopratama1 <a.ajopratama01@gmail.com>
2024-06-03 15:05:57 +0700

    Menambahkan blok catch untuk menangani pengecualian tipe int

--- manualexception.cpp
+++ manualexception.cpp
@@ -6,4 +6,8 @@ int main() {
         cout << "Selamat Belajar di prodi TI UMY " << endl;
         cout << "Pernyataan tidak akan dieksekusi" << endl;
     }
-   
+    catch (int a) {
+        cout << "Pengecualian akan dieksekusi " << endl;
+    }
+  
+}
