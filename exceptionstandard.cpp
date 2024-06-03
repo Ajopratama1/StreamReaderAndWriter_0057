70a358dea543c07f7aceea33a2c260c66dedf603
Ajopratama1 <a.ajopratama01@gmail.com>
2024-06-03 15:15:15 +0700

    Menambahkan pesan awal program dan percobaan akses elemen array dengan penanganan pengecualian

--- exceptionstandard.cpp
+++ exceptionstandard.cpp
@@ -3,4 +3,10 @@
 #include <array>
 using namespace std;
 
-
+int main (){
+    cout << "Awal Program " << endl;
+    try {
+        array<int, 3> data = {8, 4, 2};
+        cout<<data.at(5)<<endl;
+    }
+   
