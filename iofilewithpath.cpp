de94774e8ad596cd09a0b8eaaed1687c4e851c21
Ajopratama1 <a.ajopratama01@gmail.com>
2024-06-03 14:51:22 +0700

    mnambahkan awalan coding

--- iofilewithpath.cpp
+++ iofilewithpath.cpp
@@ -3,17 +3,3 @@
 #include <string>
 using namespace std;
 
-int main() {
-    string baris;
-    string NamaFile;
-
-    cout << "Masukan Nama File : ";
-    cin >> NamaFile;
-    ofstream outfile;
-    outfile.open(NamaFile + ".txt", ios::out);
-
-    while(true) {
-        cout << "-";
-    }
-
-}
