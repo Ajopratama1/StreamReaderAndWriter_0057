4fae4dca9457fefa2c7a19b7d83b81cfe55e8091
Ajopratama1 <a.ajopratama01@gmail.com>
2024-06-03 15:15:50 +0700

    Menambahkan penanganan pengecualian berbasis objek dan pesan akhir program

--- exceptionstandard.cpp
+++ exceptionstandard.cpp
@@ -9,4 +9,8 @@ int main (){
         array<int, 3> data = {8, 4, 2};
         cout<<data.at(5)<<endl;
     }
-   
+    catch (exception& e) {
+        cout << e.what() << endl;
+    }
+    cout << "Baris Program Yang terakhir" << endl;
+}
